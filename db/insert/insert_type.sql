9ae72aab091985d8614d911fe2c59e7ffa9cdfee
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-11-16 10:11:38 -0800

    insert sqls

---
+++ db/insert/insert_type.sql
@@ -0,0 +1,17 @@
+INSERT INTO Type(name) VALUES ('Bug');
+INSERT INTO Type(name) VALUES ('Dragon');
+INSERT INTO Type(name) VALUES ('Ice');
+INSERT INTO Type(name) VALUES ('Fighting');
+INSERT INTO Type(name) VALUES ('Fire');
+INSERT INTO Type(name) VALUES ('Flying');
+INSERT INTO Type(name) VALUES ('Grass');
+INSERT INTO Type(name) VALUES ('Ghost');
+INSERT INTO Type(name) VALUES ('Ground');
+INSERT INTO Type(name) VALUES ('Electric');
+INSERT INTO Type(name) VALUES ('Normal');
+INSERT INTO Type(name) VALUES ('Poison');
+INSERT INTO Type(name) VALUES ('Psychic');
+INSERT INTO Type(name) VALUES ('Rock');
+INSERT INTO Type(name) VALUES ('Water');
+INSERT INTO Type(name) VALUES ('Steel');
+INSERT INTO Type(name) VALUES ('Fairy');
