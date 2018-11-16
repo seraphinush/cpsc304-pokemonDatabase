8368371257805bf243947285e17513b0bc799c53
JDAV96 <arevalojose2015@outlook.com>
2018-11-16 12:55:23 -0800

    adding sqlutil.php and began working on signup.php -Jose

--- php/signup.php
+++ php/signup.php
@@ -1,3 +1,20 @@
 <?php
+include 'sqlutil.php'
 
+$db_conn = connectToDB("ora_l8o0b", "a33250151");
+
+if ($db_conn) {
+	if (array_key_exists('insertuser', $_POST)) {
+		//Getting the values from user and insert data into the table
+			$tuple = array (
+				":bind1" => $_POST['accName'],
+				":bind2" => $_POST['accPassword'],
+			);
+			$alltuples = array (
+				$tuple
+			);
+			executeBoundSQL("insert into Trainers values (:bind1, :bind2)", $alltuples);
+			OCICommit($db_conn);
+	}
+}
 >
