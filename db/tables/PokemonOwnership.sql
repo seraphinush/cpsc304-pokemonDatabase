299eda44ecc2581aa2e1003bcef078280c8a6cdf
JDAV96 <arevalojose2015@outlook.com>
2018-11-15 23:29:06 -0800

    Fixing PokemonOwnership.sql -Jose

--- db/tables/PokemonOwnership.sql
+++ db/tables/PokemonOwnership.sql
@@ -3,7 +3,7 @@ Pokemon_id		INTEGER,
 Trainer_id		INTEGER,
 PRIMARY KEY (Pokemon_id, Trainer_id),
 FOREIGN KEY (Pokemon_id)
-  REFERENCES Pokemon(id)
+  REFERENCES PokemonInstance(id)
   ON DELETE CASCADE,
 FOREIGN KEY (Trainer_id)
   REFERENCES Trainer(id)
