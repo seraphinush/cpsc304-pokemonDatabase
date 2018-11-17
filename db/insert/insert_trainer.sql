67b99cc50852e62a49e74404051f8d2fbf6bcb80
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-11-16 17:26:38 -0800

    trainer insert and csv

---
+++ db/insert/insert_trainer.sql
@@ -0,0 +1,10 @@
+INSERT INTO Trainer(id,name,password) VALUES (1,'Seraph',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (2,'Jose',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (3,'Leticia',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (4,'Ofir',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (5,'Ed',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (6,'Jojo',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (7,'Lad',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (8,'Chubaka',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (9,'Jana',12345678);
+INSERT INTO Trainer(id,name,password) VALUES (10,'Max',12345678);
