65996a2bed81f03fa5685f2c6fce5d61e450a41a
seraphinush <seraphinus.hong@gmail.com>
2018-11-16 22:21:20 -0800

    test 1

--- php/signup.php
+++ php/signup.php
@@ -1,7 +1,7 @@
 <?php
 
 $success = True; //keep track of errors so it redirects the page only if there are no errors
-$db_conn = OCILogon("ora_l8o0b", "a33250151", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+$db_conn = OCILogon("ora_v9m8", "a38134110", "dbhost.ugrad.cs.ubc.ca:1522/ug");
 
 function executeBoundSQL($cmdstr, $list) {
 	/* Sometimes the same statement will be executed for several times ... only
@@ -40,19 +40,50 @@ function executeBoundSQL($cmdstr, $list) {
 	}
 }
 
+function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+		// connection handle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+
+}
+
 if ($db_conn) {
 	if (array_key_exists('signup', $_POST)) {
 		//Getting the values from user and insert data into the table
+			$maxID = executePlainSQL("SELECT MAX(id) FROM Trainer");
 			$tuple = array (
 				":bind1" => $_POST['accUsername'],
 				":bind2" => $_POST['accPassword'],
+				":bind3" => $maxID + 1
 			);
 			$alltuples = array (
 				$tuple
 			);
-			executeBoundSQL("insert into Trainer values (:bind1, :bind2)", $alltuples);
+			executeBoundSQL("insert into Trainer values (:bind3, :bind1, :bind2)", $alltuples);
 			OCICommit($db_conn);
 	}
-	echo "test"
 }
+
+echo "SUCCESS";
+header("url: index.html?sent=true");
+
 ?>
