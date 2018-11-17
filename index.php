fefd0ef6eb609ff14c4b771de452275c65b6d4b0
JDAV96 <arevalojose2015@outlook.com>
2018-11-16 17:04:36 -0800

    testo -Jose

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
 </html>
 
 <?php
-
+/*
 $success = True; //keep track of errors so it redirects the page only if there are no errors
 $db_conn = OCILogon("ora_l8o0b", "a33250151", "dbhost.ugrad.cs.ubc.ca:1522/ug");
 
@@ -117,7 +117,7 @@ function executeBoundSQL($cmdstr, $list) {
 	 In this case, you don't need to create the statement several times; 
 	 using bind variables can make the statement be shared and just parsed once.
 	 This is also very useful in protecting against SQL injection.  
-      See the sample code below for how this functions is used */
+      See the sample code below for how this functions is used 
 
 	global $db_conn, $success;
 	$statement = OCIParse($db_conn, $cmdstr);
@@ -135,7 +135,6 @@ function executeBoundSQL($cmdstr, $list) {
 			//echo "<br>".$bind."<br>";
 			OCIBindByName($statement, $bind, $val);
 			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-
 		}
 		$r = OCIExecute($statement, OCI_DEFAULT);
 		if (!$r) {
@@ -162,5 +161,5 @@ if ($db_conn) {
 			OCICommit($db_conn);
 	}
 	echo "test"
-}
+}*/
 ?>
