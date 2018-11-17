5ec784613474ee9bc79dcb4ed6476e1583078f64
José Arévalo <l8o0b@anvil.ugrad.cs.ubc.ca>
2018-11-16 19:23:14 -0800

    finally fixed the php thing -Jose

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
 <?php
 $success = True; //keep track of errors so it redirects the page only if there are no errors
 $db_conn = OCILogon("ora_l8o0b", "a33250151", "dbhost.ugrad.cs.ubc.ca:1522/ug");
-/*
+
 function executeBoundSQL($cmdstr, $list) {
 
 	global $db_conn, $success;
@@ -138,21 +138,49 @@ function executeBoundSQL($cmdstr, $list) {
 			$success = False;
 		}
 	}
-}*/
+}
+
+function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+	//echo "<br>running ".$cmdstr."<br>";
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
+		// connection handle
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+
+}
 
 if ($db_conn) {
 	if (array_key_exists('signup', $_POST)) {
 		//Getting the values from user and insert data into the table
+			$maxID = executePlainSQL("SELECT MAX(id) FROM Trainer");
 			$tuple = array (
 				":bind1" => $_POST['accUsername'],
 				":bind2" => $_POST['accPassword'],
+				":bind3" => $maxID + 1
 			);
 			$alltuples = array (
 				$tuple
 			);
-			//executeBoundSQL("insert into Trainer values (:bind1, :bind2)", $alltuples);
-			//OCICommit($db_conn);
+			executeBoundSQL("insert into Trainer values (:bind3, :bind1, :bind2)", $alltuples);
+			OCICommit($db_conn);
+		echo "test";
 	}
-	echo "test";
 }
 ?>
