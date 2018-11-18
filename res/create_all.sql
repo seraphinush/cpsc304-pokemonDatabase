6ca70bb5d9925e3c7446cf0039f27905f37c12ac
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-11-17 16:42:23 -0800

    insert and create all

---
+++ res/create_all.sql
@@ -0,0 +1,15 @@
+START table_species;
+START table_pInstance;
+START table_levelThreshold;
+START table_trainer;
+START table_location;
+START table_biome;
+START table_type;
+START table_move;
+START table_move_species;
+START table_species_type;
+START table_evolutions;
+START table_type_biome;
+START table_strength;
+START table_biome_location;
+START table_pOwnership;
