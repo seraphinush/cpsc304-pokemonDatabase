58a933f63b2e5043446f7298c98136ac532e0a0c
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-11-18 10:23:29 -0800

    fixed pOwnership

--- res/table_pOwnership.sql
+++ res/table_pOwnership.sql
@@ -1,7 +1,7 @@
 CREATE TABLE PokemonOwnership (
 Pokemon_id		INTEGER, 
 Trainer_id		INTEGER,
-is_stored     	BOOLEAN,
+is_stored     	BIT,
 PRIMARY KEY (Pokemon_id, Trainer_id),
 FOREIGN KEY (Pokemon_id)
   REFERENCES PokemonInstance(id)
