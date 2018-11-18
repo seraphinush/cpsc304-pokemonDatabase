994eb4164b600f8afdd9e7b4f3339c6a4113d883
Ofir <t0n2b@ugrad.cs.ubc.ca>
2018-11-17 17:55:41 -0800

    better precision

--- res/table_species.sql
+++ res/table_species.sql
@@ -2,8 +2,8 @@ CREATE TABLE Species (
 name 			CHAR(10),
 rarity 			INTEGER,
 description 	CHAR(255), 
-minHeight 		DECIMAL(3,2),
-maxHeight 		DECIMAL(3,2),
-minWeight 		DECIMAL(3,2),
-maxWeight 		DECIMAL(3,2),
+minHeight 		DECIMAL(18,2),
+maxHeight 		DECIMAL(18,2),
+minWeight 		DECIMAL(18,2),
+maxWeight 		DECIMAL(18,2),
 PRIMARY KEY (name));
