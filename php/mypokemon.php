8e832cf0b179fd11815960d0cdbd9b9d908db12d
José Arévalo <l8o0b@anvil.ugrad.cs.ubc.ca>
2018-11-18 19:02:57 -0800

    My pokemon -Jose

--- php/mypokemon.php
+++ php/mypokemon.php
@@ -1,3 +1,8 @@
+<?php
+	ini_set('session.save_path', getcwd() . "/../../../public_html_sessions");
+	$start = session_start(); 
+?>
+
 <html>
 
 <head>
@@ -41,7 +46,39 @@
         <!-- CONTENT -->
         <div id="content">
             <div id="mypokemon">
-                <p>MYPOKEMON STUB</p>
+                <?php
+include 'dbmanager.php';
+$manager = DBManager::Instance();
+
+function printResult($result) { //prints results from a select statement
+	echo "<table>";
+	echo "<tr><th>ID</th><th>Name</th></tr>";
+
+	while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+		echo "<tr><td>" . $row["ID"] . "</td><td>" . $row["NICKNAME"] . "</td></tr>"; //or just use "echo $row[0]" 
+	}
+	echo "</table>";
+
+}
+
+	if (isset($_SESSION['ID'])) {
+		try {
+			$tmpid = $_SESSION['ID'];
+			$result = $manager->executePlainSQL("SELECT I.ID, I.nickname FROM PokemonInstance I, PokemonOwnership O WHERE I.id = O.Pokemon_id AND O.Trainer_id = '$tmpid' AND O.is_stored = 0");
+			if ($result) {
+				printResult($result);
+			} else {
+				echo "<font color='E69F00'>Unsuccessful.</font>";
+			}
+			OCICommit($db_conn);
+		} catch (Exception $e) {
+			echo htmlentities($e['message']);
+		}
+	} else {
+		echo "Please sign in or create an account to see your active pokemon!";
+	}
+
+		?>
             </div>
         </div>
 
@@ -52,82 +89,3 @@
 </body>
 
 </html>
-
-<?php
-$success = true; // error flag
-$db_conn = OCILogon("ora_v9m8", "a38134110", "dbhost.ugrad.cs.ubc.ca:1522/ug");
-
-function executeBoundSQL($cmdstr, $list) {
-
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr);
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn);
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	foreach ($list as $tuple) {
-		foreach ($tuple as $bind => $val) {
-			//echo $val;
-			//echo "<br>".$bind."<br>";
-			OCIBindByName($statement, $bind, $val);
-			unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
-		}
-		$r = OCIExecute($statement, OCI_DEFAULT);
-		if (!$r) {
-			echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-			$e = OCI_Error($statement); // For OCIExecute errors pass the statement handle
-			echo htmlentities($e['message']);
-			echo "<br>";
-			$success = False;
-		}
-	}
-}
-
-function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
-	//echo "<br>running ".$cmdstr."<br>";
-	global $db_conn, $success;
-	$statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
-
-	if (!$statement) {
-		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
-		$e = OCI_Error($db_conn); // For OCIParse errors pass the       
-		// connection handle
-		echo htmlentities($e['message']);
-		$success = False;
-	}
-
-	$r = OCIExecute($statement, OCI_DEFAULT);
-	if (!$r) {
-		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
-		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
-		echo htmlentities($e['message']);
-		$success = False;
-	} else {
-
-	}
-	return $statement;
-
-}
-
-if ($db_conn) {
-	if (array_key_exists('signup', $_POST)) {
-		//Getting the values from user and insert data into the table
-			$maxID = executePlainSQL("SELECT MAX(id) FROM Trainer");
-			$tuple = array (
-				":bind1" => $_POST['accUsername'],
-				":bind2" => $_POST['accPassword'],
-				":bind3" => $maxID + 1
-			);
-			$alltuples = array (
-				$tuple
-			);
-			executeBoundSQL("insert into Trainer values (:bind3, :bind1, :bind2)", $alltuples);
-			OCICommit($db_conn);
-		echo "test";
-	}
-}
-?>
